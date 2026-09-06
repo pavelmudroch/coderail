476b60bba76c7b16874a74ccdbf10cbddad6ca4f
Pavel Mudroch <xmudrochpavel@gmail.com>
2026-09-06 12:56:25 +0200

    feat(utils/md): add markdown helper utilities

---
+++ lib/utils/md.sh
@@ -0,0 +1,167 @@
+#!/usr/bin/env sh
+
+md_is_frontmatter_valid()
+{
+    if [ -z "$1" ]; then
+        echo "Missing starting \"---\""
+        exit 1
+    fi
+
+    echo "$1" | awk '
+        NR == 1 {
+            if ($0 != "---") {
+                error = 2
+                exit 1
+            }
+
+            in_frontmatter = 1
+            error = 1
+            next
+        }
+
+        in_frontmatter && $0 == "---" {
+            in_frontmatter = 0
+            error = 0
+            exit 0
+        }
+
+        in_frontmatter {
+            if ($0 == "") {
+                next
+            }
+
+            colon = index($0, ":")
+
+            if (colon == 0) {
+                print "Malformed key:value at line " NR ": " $0
+                error = -1
+                exit 1
+            }
+
+            key = substr($0, 1, colon - 1)
+
+            if (key !~ /^[a-z][a-z0-9-]*$/) {
+                print "Invalid key \"" key "\" at line " NR
+                error = -1
+                exit 1
+            }
+
+            if (seen[key] == 1) {
+                print "Duplicate key \"" key "\" at line " NR
+                error = -1
+                exit 1
+            }
+
+            seen[key] = 1
+            next
+        }
+
+        END {
+            if (NR == 0) {
+                print "Missing starting \"---\""
+                exit 1
+            }
+
+            if (error != 0) {
+                if (error == 1) {
+                    print "Missing ending \"---\""
+                }
+                if (error == 2) {
+                    print "Missing starting \"---\""
+                }
+                exit 1
+            }
+        }
+    '
+}
+
+md_frontmatter_get()
+{
+    key="$1"
+
+    awk -v key="$key" '
+        BEGIN {
+            in_frontmatter = 1
+        }
+
+        NR == 1 {
+            next
+        }
+
+        in_frontmatter && $0 == "---" {
+            exit found ? 0 : 1
+        }
+
+        in_frontmatter {
+            pos = index($0, ":")
+            if (pos == 0)
+                exit 1
+
+            current_key = substr($0, 1, pos - 1)
+
+            if (current_key == key) {
+                value = substr($0, pos + 1)
+                sub(/^[[:space:]]*/, "", value)
+                print value
+                found = 1
+            }
+        }
+
+        END {
+            if (found != 1) {
+                exit 1
+            }
+        }
+    '
+}
+
+md_frontmatter_set() {
+    key=$1
+    value=$2
+
+    awk -v key="$key" -v value="$value" '
+        BEGIN {
+            in_frontmatter = 1
+        }
+
+        NR == 1 {
+            print
+            next
+        }
+
+        in_frontmatter {
+            if ($0 == "---") {
+                if (!found) {
+                    print key ": " value
+                }
+
+                print
+                in_frontmatter = 0
+                next
+            }
+
+            separator = index($0, ":")
+            if (separator) {
+                line_key = substr($0, 1, separator - 1)
+
+                if (line_key == key) {
+                    print key ": " value
+                    found = 1
+                    next
+                }
+            }
+
+            print
+            next
+        }
+
+        {
+            print
+        }
+    '
+}
+
+md_frontmatter_empty()
+{
+    printf '%s\n%s\n' "---" "---"
+}
